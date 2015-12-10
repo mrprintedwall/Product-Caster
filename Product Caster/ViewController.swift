b00236e9d0c961448f67a4a66e5965d73beb3be8
John Pili <mrprintedwall@gmail.com>
2015-12-11 02:32:45 +0800

    Initial Commit

---
+++ Product Caster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Product Caster
+//
+//  Created by John Pili on 12/11/15.
+//  Copyright © 2015 John Pili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
